0ba614d74a346e74de4e0a1dcfbe0a6a10b4833c
bobtal <talevski.boban@gmail.com>
2019-01-26 19:07:35 +0100

    Removes the sample and unneeded endpoint from MyEndpoint.java

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -19,15 +19,6 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
-        return response;
-    }
-
     /** Endpoint method that fetches a joke from
      * the javaJokes java library and returns it */
     @ApiMethod(name = "fetchJoke")
